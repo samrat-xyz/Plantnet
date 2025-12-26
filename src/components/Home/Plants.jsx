92dfc08d0a8d436177a821516fbc8a97a9c0b509
Asif-vai <mr3366533@gmail.com>
2025-12-26 11:33:37 +0600

    fetching data by useQuery

--- src/components/Home/Plants.jsx
+++ src/components/Home/Plants.jsx
@@ -1,25 +1,28 @@
-import Card from './Card'
-import Container from '../Shared/Container'
-
+import Card from "./Card";
+import Container from "../Shared/Container";
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+import LoadingSpinner from "../Shared/LoadingSpinner";
 const Plants = () => {
+  const { isLoading, data } = useQuery({
+    queryKey: ["plants"],
+    queryFn: async () => {
+      const result = await axios(`${import.meta.env.VITE_API_URL}/plants`);
+      return result.data
+    },
+  });
+  console.log(data)
+  if(isLoading) return <LoadingSpinner></LoadingSpinner>
+
   return (
     <Container>
-      <div className='pt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8'>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+      <div className="pt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+        {
+          data.map(plant =><Card plant={plant} key={plant._id}/>)
+        }
       </div>
     </Container>
-  )
-}
+  );
+};
 
-export default Plants
+export default Plants;
