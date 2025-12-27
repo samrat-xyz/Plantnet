9cac692838c5c4651d67bd033956dfac6f43b8f3
Asif-vai <mr3366533@gmail.com>
2025-12-27 18:35:11 +0600

    conditionally display dashboard by role

--- src/hooks/useRole.jsx
+++ src/hooks/useRole.jsx
@@ -1,18 +1,20 @@
-import React from "react";
 import useAuth from "./useAuth";
 import useAxiosSecure from "./useAxiosSecure";
 import { useQuery } from "@tanstack/react-query";
+
 function useRole() {
-  const { user, loading } = useAuth();
+  const { user,loading } = useAuth();
   const axiosSecure = useAxiosSecure();
 
   const { data: role, isLoading: isRoleLoading } = useQuery({
     queryKey: ["role", user?.email],
+    enabled: !!user?.email, // ⬅️ important
     queryFn: async () => {
-      const { data } = await axiosSecure(`/users/role/${user?.email}`);
+      const { data } = await axiosSecure.get(`/users/role/${user.email}`);
       return data.role;
     },
   });
+
   return [role, isRoleLoading];
 }
 
